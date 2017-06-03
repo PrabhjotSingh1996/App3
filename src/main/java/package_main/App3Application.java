94d4c9594120a6ed6491b8d4255f1a0a159343f7
prabhjotsingh1996 <prabhjot.singh1996@gmail.com>
2017-06-03 19:13:36 +0530

    App3

---
+++ src/main/java/package_main/App3Application.java
@@ -0,0 +1,12 @@
+package package_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App3Application.class, args);
+	}
+}
